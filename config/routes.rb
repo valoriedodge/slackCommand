23011c5093e25a245d49a6475a895ed253a5d2fc
valoriedodge <valoriejones@gmail.com>
2016-12-11 14:43:54 -0800

    welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   namespace :api, defaults: { format: :json } do
     #  match '/events', to: 'events#preflight', via: [:options]
      resources :events, only: [:create]
-   end
+  end
+
+  root 'welcome#index'
 end
