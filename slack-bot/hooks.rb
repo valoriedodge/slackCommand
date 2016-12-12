1268d06f0084d115d9c1dd95e6239b224b74a73d
valoriedodge <valoriejones@gmail.com>
2016-12-12 01:05:55 -0800

    hello

---
+++ slack-bot/hooks.rb
@@ -0,0 +1,8 @@
+module MyBot
+  class MyServer < SlackRubyBot::Server
+    on 'hello' do |client, data|
+      # data['user']['id'] contains the user ID
+      client.say(text: 'hello', channel: data.channel)
+    end
+  end
+end
