eeb5b351c381d2f7bb17cae0aa5ab6f1bc1a5cb1
valoriedodge <valoriejones@gmail.com>
2016-12-11 17:44:46 -0800

    test

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -15,8 +15,12 @@ class API::EventsController < ApplicationController
     #  registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     #  if registered_application == nil
     mymessage = {
-      user: => params.user_id,
-      text: params.text
+        "text": "It's 80 degrees right now.",
+        "attachments": [
+            {
+                "text":"Partly cloudy today and tomorrow"
+            }
+        ]
     }
     render json: mymessage, status: 200
     p params
