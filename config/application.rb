209e4bdcd32d798733a5f2cbedbc1c70e0545ade
valoriedodge <valoriejones@gmail.com>
2016-12-12 00:11:41 -0800

    changes

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ Bundler.require(*Rails.groups)
 module SlackCommand
   class Application < Rails::Application
 
-    Slack.configure do |config|
-      config.token = ENV['SLACK_API_TOKEN']
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
