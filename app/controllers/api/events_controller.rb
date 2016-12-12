ae02994856e89c828d3f858dd6c21b320fbd7fea
valoriedodge <valoriejones@gmail.com>
2016-12-12 09:28:07 -0800

    revert

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -14,7 +14,7 @@ class API::EventsController < ApplicationController
    def create
     #  registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     #  if registered_application == nil
-    if params[:token] == TEpHGQVbeAMK8y93b34Tw40c
+    # if params[:token] == TEpHGQVbeAMK8y93b34Tw40c
       username = params[:token]
       mymessage = {
           "text": username,
@@ -25,9 +25,9 @@ class API::EventsController < ApplicationController
           ]
       }
       render json: mymessage, status: 200
-    else
-      render json: {error: "Token is invalid", status: 400}, status: 400
-    end
+    # else
+    #   render json: {error: "Token is invalid", status: 400}, status: 400
+    # end
     #  else
     #    @event = Event.new(event_params)
     #    @event.registered_application = registered_application
--- slack-bot/tracker.rb
+++ slack-bot/tracker.rb
@@ -3,10 +3,6 @@ module SlackBot
     command 'ping' do |client, data, match|
       client.say(text: 'ponging', channel: data.channel)
     end
-    
-    on 'bot_added' do |client, data|
-      # data['user']['id'] contains the user ID
-      client.say(text: 'hello', channel: data.channel)
-    end
+  
   end
 end
